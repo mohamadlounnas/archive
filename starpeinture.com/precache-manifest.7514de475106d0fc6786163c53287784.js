a994459078c8a8adfe8fa8dc706674aa13f9f177
mohamadlounnas <mohamadlounnas@gmail.com>
2023-04-28 15:56:58 +0100

    add starepeinture.com

---
+++ starpeinture.com/precache-manifest.7514de475106d0fc6786163c53287784.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3e5f93baa428aedddb",
+    "url": "/css/app.cee3f80d.css"
+  },
+  {
+    "revision": "bb20cd10d2768d6194df70f6becd7b70",
+    "url": "/img/applogo.bb20cd10.svg"
+  },
+  {
+    "revision": "8db4fe71c4e72d470776f8cd45883278",
+    "url": "/img/bdns4.8db4fe71.webp"
+  },
+  {
+    "revision": "c073808ac28f5a48db9dcafbccc8731d",
+    "url": "/img/bg-1@1000x-80-min.c073808a.webp"
+  },
+  {
+    "revision": "bffbcee93ea322c978f39ed0bf7ef953",
+    "url": "/img/bg-2@1000x-80-min.bffbcee9.webp"
+  },
+  {
+    "revision": "157728157488beda8a183e1d2191eb95",
+    "url": "/img/bg-3@1000x-80-min.15772815.webp"
+  },
+  {
+    "revision": "23ae47fba45f59639f9c8fbf4807e498",
+    "url": "/img/bg-4@1000x-80-min.23ae47fb.webp"
+  },
+  {
+    "revision": "a8cce258f1bd5fa3a4a8fa64d410fbde",
+    "url": "/img/divider.a8cce258.svg"
+  },
+  {
+    "revision": "72f7ae466a323861786cdde1b9c3c629",
+    "url": "/img/ecowall.72f7ae46.webp"
+  },
+  {
+    "revision": "28bc0450e1ed1d0fb6799f87b9f1ab72",
+    "url": "/img/extra.28bc0450.webp"
+  },
+  {
+    "revision": "74b0d9dff2c443f0a922f8ba04f0fe37",
+    "url": "/img/facebook.74b0d9df.svg"
+  },
+  {
+    "revision": "64d350dbaae410bbefded131662eb659",
+    "url": "/img/fixastar.64d350db.webp"
+  },
+  {
+    "revision": "37120c6ba421e7ebe9fc97dbfde086a3",
+    "url": "/img/img-1.37120c6b.svg"
+  },
+  {
+    "revision": "d18a636941df0eef833c28103b092a00",
+    "url": "/img/img-2-min.d18a6369.webp"
+  },
+  {
+    "revision": "52fb2d15fb2fe922b8c85a3fd5cd9561",
+    "url": "/img/instagram.52fb2d15.svg"
+  },
+  {
+    "revision": "5580fd2aca2ec403a50930df401eb9f5",
+    "url": "/img/klassikt.5580fd2a.webp"
+  },
+  {
+    "revision": "89b019a0eb2e3a5936916d218c005260",
+    "url": "/img/logo.89b019a0.png"
+  },
+  {
+    "revision": "2b279b9cc6256d5282f28a1ff9f5b11f",
+    "url": "/img/menu.2b279b9c.svg"
+  },
+  {
+    "revision": "f5fbe1272cd8ff57e58cd424ef3f0dc9",
+    "url": "/img/menu_close.f5fbe127.svg"
+  },
+  {
+    "revision": "7c8b61725ff809c1babadaea41d4a5a3",
+    "url": "/img/messenger.7c8b6172.svg"
+  },
+  {
+    "revision": "0d83c5ffe68f51553778c9dac7c9d9dc",
+    "url": "/img/next.0d83c5ff.svg"
+  },
+  {
+    "revision": "7b986b28bca03caaa6f0f057a1c1fbdc",
+    "url": "/img/open.7b986b28.svg"
+  },
+  {
+    "revision": "5b68f3d450244d5398d49e08b182adf3",
+    "url": "/img/paint-x-white.5b68f3d4.svg"
+  },
+  {
+    "revision": "fe6794c0b7e4e25ab307243b046939b4",
+    "url": "/img/paint-x.fe6794c0.svg"
+  },
+  {
+    "revision": "383b61536de16748ee12296b48184e92",
+    "url": "/img/paint.383b6153.svg"
+  },
+  {
+    "revision": "5ab0c2a9dd4e27027116d5113495cefd",
+    "url": "/img/search.5ab0c2a9.svg"
+  },
+  {
+    "revision": "cedf40f6aa770931aba928fe5fed83a0",
+    "url": "/img/titles-bg.cedf40f6.svg"
+  },
+  {
+    "revision": "7ed43415c36cd63e3507aa0d6e08cf5b",
+    "url": "/img/whatsapp.7ed43415.svg"
+  },
+  {
+    "revision": "6d5b906a35c3c1a86a75562605074bb2",
+    "url": "/img/wonder-white-logo.6d5b906a.webp"
+  },
+  {
+    "revision": "170a9004299173d91812780599a6f7ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c3e5f93baa428aedddb",
+    "url": "/js/app.f8d96d01.js"
+  },
+  {
+    "revision": "0a058d5084a474e70cdb",
+    "url": "/js/chunk-vendors.8a85b6a8.js"
+  },
+  {
+    "revision": "060e7f112b6e9355ba8828ba8dca8617",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ starpeinture.com/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.7514de475106d0fc6786163c53287784.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "STARPEINTURE"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
